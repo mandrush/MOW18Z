b898cf02bb9522e69618b5c98432026e67156749
mandrush <andruszkiewiczm1@gmail.com>
2019-01-13 18:51:57 +0100

    random experimentations + RSE and summaries

--- main.R
+++ main.R
@@ -29,5 +29,17 @@ inTrain <- createDataPartition(y = data$Dalc,
 training <- data[ inTrain,]
 testing <- data[-inTrain,]
 
-#weekModel <- lm(Walc ~ school+sex+age+address+famsize+Pstatus+Medu+Fedu+Mjob+Fjob+reason+guardian+traveltime+studytime+failures+schoolsup+famsup+paid+activities+nursery+higher+internet+romantic+famrel+freetime+goout+health+absences+G1+G2+G3,
-#                     data=training)
+#Walc model; depending on every possible attribute except for Dalc
+weekModel <- lm(Walc ~ . -Dalc , data=training)
+summary(weekModel)
+#plot(weekModel)
+
+# the overall quality of the model can be assessed by examining the R-squared (R2) and Residual Standard Error (RSE)
+#RSE: a measure of error of prediction. The lower the RSE, the more accurate the model (on the data in hand).
+sigma(weekModel)/mean(data$Walc) #approx. 0.48, kind of bad
+
+weekNoG1 <- lm(Walc ~ . -Dalc -G1 , data=training)
+summary(weekNoG1)
+sigma(weekNoG1)/mean(data$Walc) #approx. 0.48, G1 doesn't havey any significance?
+
+
