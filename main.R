97cd31252827338a68549f0edaead565035f3dcd
mandrush <andruszkiewiczm1@gmail.com>
2019-01-19 00:04:51 +0100

    pca

--- main.R
+++ main.R
@@ -23,17 +23,100 @@ paramsPor <- paramsPor[complete.cases(paramsPor), ]
 #joined data
 data <- rbind(paramsMat, paramsPor)
 duplicated(data)
+
+
 #correlation diagram
 #corrgram(data, lower.panel=panel.shade, upper.panel=panel.ellipse)
 corMatrix <- cor(data)
 corrplot(corMatrix, method='circle')
 
 #partition the data (p = ratio between training and the testing data)
-inTrain <- createDataPartition(y = data$Dalc,
+inTrain <- createDataPartition(y = data$school,
                                p = 0.3,
                                list = FALSE)
 training <- data[ inTrain,]
 testing <- data[-inTrain,]
+
+###### PCA #######
+
+data <- sapply( data, as.numeric )
+
+trainingPCA <- data[ inTrain,]
+testingPCA <- data[-inTrain,]
+
+#training <- sapply( training, as.numeric )
+#testing <- sapply( testing, as.numeric )
+
+trainingPCA <- subset(trainingPCA, select = -c(Walc))
+testingPCA <- subset(testingPCA, select = -c(Walc))
+
+#PCA IMPLEMENTATION
+# a) prcomp method
+prin_comp <- prcomp(trainingPCA, scale. = T)
+
+# b) compute standard deviation of each principal component
+std_dev <- prin_comp$sdev
+
+# c) compute variance
+pr_var <- std_dev^2
+
+# d) proportion of variance explained
+prop_varex <- pr_var/sum(pr_var)
+
+
+#scree plot
+plot(prop_varex, xlab = "Principal Component",
+     ylab = "Proportion of Variance Explained",
+     type = "b")
+
+
+#cumulative scree plot
+plot(cumsum(prop_varex), xlab = "Principal Component",
+     ylab = "Cumulative Proportion of Variance Explained",
+     type = "b")
+
+#WE ARE INTERESTED ONLY IN PCAs which have impact of 90% overall  1st step
+PCA_impact = 0.7
+i=1;
+while (i < length(prop_varex)) {
+  if(cumsum(prop_varex)[i] >= PCA_impact) break;
+  i = i+1;
+}
+which_component = i;
+
+
+##### END OF PCA ######
+
+
+### model ###
+
+#PRINCIPAL COMPONENTS + FRAUDFLAG COLUMN FOR LEARNING ALGORITHM
+training <- data.frame(Walc = training$Walc, prin_comp$x)
+
+#WE ARE INTERESTED ONLY IN PCAs which have impact of 90% overall   2nd step
+training <- training[,1:(which_component+1)]
+
+#DECISION TREE
+tree <- rpart(Walc ~ . , data = training, method = "class")
+
+#TRANSFORM TEST DATA INTO PCA
+testingPCA <- predict(prin_comp, newdata = testingPCA)
+testingPCA <- as.data.frame(testingPCA)
+
+#WE ARE INTERESTED ONLY IN PCAs which have impact of 90% overall
+testingPCA <- testingPCA[,1:(which_component)]
+
+#make prediction on test data
+predicted <- predict(tree, testingPCA, type = "class")
+
+table(predicted, testing$Walc)
+
+
+
+
+
+
+
 
 #Walc model; depending on every possible attribute except for Dalc
 #weekModel <- lm(factor(Walc) ~ . -Dalc , data=training)
@@ -48,9 +131,10 @@ testing <- data[-inTrain,]
 #weekNoG1 <- lm(Walc ~ . -Dalc -G1 , data=training)
 #summary(weekNoG1)
 #sigma(weekNoG1)/mean(data$Walc) #approx. 0.48, G1 doesn't havey any significance?
-#training <- subset(training, select = -c(school))
 
-tree <- rpart(Walc ~ . ,data = training, method = "class")
-predicted <- predict(tree, newdata = testing, type = "class")
+#training <- subset(training, select = -c(school, Mjob, Fjob, address, famsize, Pstatus, reason, guardian ))
 
-table(predicted, testing$Walc)
+#tree <- rpart(Walc ~ . ,data = training, method = "class")
+#predicted <- predict(tree, newdata = testing, type = "class")
+
+
